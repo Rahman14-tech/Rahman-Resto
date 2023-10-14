99009805025f0b4e172f1aa4814b21638fa59b3e
Rahman14-tech <80510054+Rahman14-tech@users.noreply.github.com>
2023-10-14 18:26:42 +0700

    Updated

--- store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import UserReduxReducer from "./src/reduxstate/UserRedux"
-import ProductRedux from "./src/reduxstate/ProductRedux";
-export const store = configureStore({
-    reducer:{
-        UserRedux: UserReduxReducer,
-        ProductRedux: ProductRedux
-    },
-})
